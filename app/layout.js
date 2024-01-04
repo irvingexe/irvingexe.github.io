4842ee14146ddb66b8c84b57a7844eaba0d94f73
Irving Mariscales <mariscales.irving@gmail.com>
2024-01-03 19:17:17 -0700

    Metatags

--- app/layout.js
+++ app/layout.js
@@ -15,25 +15,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      {/* Primary Meta Tags */}
-      <title>Irving Mariscales — Portfolio</title>
-      <meta name="title" content="Irving Mariscales — Portfolio" />
-      <meta name="description" content="I am a Front End Developer with a taste for design and interactivity. This is my web portfolio, here I present some of my experiences, feel free to take a look." />
+        {/* Primary Meta Tags */}
+        <title>Irving Mariscales — Portfolio</title>
+        <meta name="title" content={metadata.title} />
+        <meta name="description" content={metadata.description} />
 
-      {/* Open Graph / Facebook */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://www.irving.work/" />
-      <meta property="og:title" content="Irving Mariscales — Portfolio" />
-      <meta property="og:description" content="I am a Front End Developer with a taste for design and interactivity. This is my web portfolio, here I present some of my experiences, feel free to take a look." />
-      <meta property="og:image" content="/thumbnail.jpg" />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metadata.title} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/thumbnail.jpg" />
 
-      {/* Twitter */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://www.irving.work" />
-      <meta property="twitter:title" content="Irving Mariscales — Portfolio" />
-      <meta property="twitter:description" content="I am a Front End Developer with a taste for design and interactivity. This is my web portfolio, here I present some of my experiences, feel free to take a look." />
-      <meta property="twitter:image" content="/thumbnail.jpg" />
-
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.irving.work" />
+        <meta property="twitter:title" content={metadata.title} />
+        <meta property="twitter:description" content={metadata.description} />
+        <meta property="twitter:image" content="/thumbnail.jpg" />
 
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
